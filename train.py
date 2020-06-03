52ac02f7f624d1736c428223206e85d7953b4e44
Nikhil <shnik456@iitkgp.ac.in>
2020-06-03 09:40:59 -0700

    added the structure to train engine

---
+++ train.py
@@ -0,0 +1,14 @@
+# all the imports go here
+
+# the train engine here
+def train_engine(config, dataset, dataset_eval=None):
+
+    # here you will get all the things ffrom the dataset init
+
+    # Next you'll have to create a Net
+
+    # Then the dataset enumerator and batching
+
+    # Optimizer then
+
+    # Calling the val function
